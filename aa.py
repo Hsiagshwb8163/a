44506eabb4f59c7c8078f28a2c46cad774cb93e5
Hsiagshwb8163 <111885216+Hsiagshwb8163@users.noreply.github.com>
2022-08-23 20:10:22 +0600

    Update aa.py

--- aa.py
+++ aa.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot("5569517557:AAHEPxk21TdyycOmAFA0jy7UWLhdMq_Q5GQ")
 @bot.message_handler(commands=['start'])
 def abc(m):
     nm = m.from_user.first_name
-    send_message(m.chat.id,"Oye "+nm+" Okajer bot e aisen ken?")
+    bot.send_message(m.chat.id,"Oye "+nm+" Okajer bot e aisen ken?")
     print(nm+" called /start")
 
 bot.polling()
